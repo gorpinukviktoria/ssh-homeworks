62b7412a5918915f61fde0319d457972cb4d17e3
unknown <=>
2022-05-13 16:15:42 +0300

    home-work

---
+++ hw-2/script.js
@@ -0,0 +1,20 @@
+let userAge = '15';
+console.log(userAge);
+
+let userName= 'Olga' ;
+console.log(userName);
+
+let userSurname = 'Grishko';
+console.log(userSurname);
+
+
+let newUserName = 'Misha';
+let shoppingList ='shopping list';
+let userOnline = 'online';
+let payForWork = 'salary';
+let cursorСoordinates = 'cursor mouse'
+
+
+const sense = 'Hello world';
+
+console.log('sense', sense);
