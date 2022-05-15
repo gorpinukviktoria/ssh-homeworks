cbe7930984867b7681f1bbc73854cad24978aec5
unknown <=>
2022-05-15 16:16:19 +0300

    hm-3

---
+++ hw-3/script.js
@@ -0,0 +1,32 @@
+// const numbers = Number(prompt("6 > 3"));
+// console.log(`numbers`, numbers);
+
+// if (numbers) {
+//     alert("6 больше 3");
+// }
+
+
+
+
+// const Min_Age = 12;
+// const visitAge = Number(prompt("Сколько вам лет?"));
+// const withParents = confirm("Вы с родителями?");
+
+// if (visitAge >= Min_Age || withParents) {
+//     console.log("Доступ разрешен");
+// } else {
+//     console.log("Возраст слишком мал для посещенния.");
+// }
+
+
+
+
+const example = Number(prompt("5 + 3 = ?"));
+
+switch (example) {
+    case 8 :
+        alert ("That's right!");
+        break;
+    default:
+        alert ("Not true!");
+}
